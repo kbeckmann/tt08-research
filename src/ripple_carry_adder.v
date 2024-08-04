ee3749bd86a81d143c2c30138f735a7fddd06dca
Konrad Beckmann <konrad.beckmann@gmail.com>
2024-08-04 10:45:37 +0200

    WIP

---
+++ src/ripple_carry_adder.v
@@ -0,0 +1,34 @@
+module ripple_carry_adder #(parameter WIDTH = 24) (
+    input wire [WIDTH-1:0] a,
+    input wire [WIDTH-1:0] b,
+    input wire cin,
+    output wire [WIDTH-1:0] sum,
+    output wire cout
+);
+    wire [WIDTH-1:0] carry;
+
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin: adder
+            if (i == 0) begin
+                full_adder fa (
+                    .a(a[i]),
+                    .b(b[i]),
+                    .cin(cin),
+                    .sum(sum[i]),
+                    .cout(carry[i])
+                );
+            end else begin
+                full_adder fa (
+                    .a(a[i]),
+                    .b(b[i]),
+                    .cin(carry[i-1]),
+                    .sum(sum[i]),
+                    .cout(carry[i])
+                );
+            end
+        end
+    endgenerate
+
+    assign cout = carry[WIDTH-1];
+endmodule
