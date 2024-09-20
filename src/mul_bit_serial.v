141904c41ee95fbfe0d7bc4ad3102d91033c5c9f
Konrad Beckmann <konrad.beckmann@gmail.com>
2024-09-20 23:34:31 +0200

    Make it a bit smaller

--- src/mul_bit_serial.v
+++ src/mul_bit_serial.v
@@ -12,7 +12,8 @@ module mul_bit_serial #(
     output reg output_valid
 );
 
-    reg [WIDTH-1:0] a_reg, b_reg;
+    // reg [WIDTH-1:0] a_reg;
+    reg [WIDTH-1:0] b_reg;
     reg [WIDTH*2-1:0] partial_product;
     reg [$clog2(WIDTH):0] bit_counter;
     reg multiplying;
@@ -20,7 +21,7 @@ module mul_bit_serial #(
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            a_reg <= 0;
+            // a_reg <= 0;
             b_reg <= 0;
             partial_product <= 0;
             bit_counter <= 0;
@@ -29,7 +30,7 @@ module mul_bit_serial #(
             output_valid <= 1'b0;
         end else begin
             if (start && !multiplying) begin
-                a_reg <= a;
+                // a_reg <= a;
                 b_reg <= b;
                 partial_product <= 0;
                 bit_counter <= 0;
@@ -37,7 +38,7 @@ module mul_bit_serial #(
                 output_valid <= 1'b0;
             end else if (multiplying) begin
                 if (b_reg[0]) begin
-                    partial_product <= partial_product + (a_reg << bit_counter);
+                    partial_product <= partial_product + (a << bit_counter);
                 end
                 b_reg <= b_reg >> 1;
                 bit_counter <= bit_counter + 1;
